5e78189ddcaff5082629c11b357323de5d03db5f
Alex Hughes <ahughesalex@gmail.com>
2016-10-05 21:25:55 -0700

    Removing compat code from __init__

--- __init__.py
+++
@@ -1,8 +0,0 @@
-try:
-    from PySide import QtCore, QtGui
-    # pyside 1 doesnt have QtWidgets
-    QtWidgets = QtGui
-
-except ImportError:
-    # Assumes PySide2
-    from PySide2 import QtCore, QtWidgets, QtGui
